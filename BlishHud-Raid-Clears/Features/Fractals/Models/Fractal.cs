c5ffb9d25b8056d450b3bf2adbb86e9132a5f83d
Soeed <a727891@gmail.com>
2023-03-19 18:13:51 -0500

    model+panel

---
+++ BlishHud-Raid-Clears/Features/Fractals/Models/Fractal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using RaidClears.Features.Shared.Models;
+
+namespace RaidClears.Features.Fractals.Models;
+
+public class Fractal : GroupModel
+{ 
+    public Fractal(string name, int index, string shortName, IEnumerable<BoxModel> boxes) : base(name, index, shortName, boxes)
+    {
+        
+    }
+    public virtual void Dispose()
+    {
+
+    }
+}
